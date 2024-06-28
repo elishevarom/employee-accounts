9cbbdc2877da5aca26829f954214e63ee70ef257
Egrosberg <ecgrosberg@gmail.com>
2024-06-27 21:11:13 -0400

    update page with hooks

--- employee-accounts-web-app/src/pages/update.jsx
+++ employee-accounts-web-app/src/pages/update.jsx
@@ -1,48 +1,71 @@
-import { Outlet, Link} from "react-router-dom"
-import Accordion from 'react-bootstrap/Accordion';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-
+import { Accordion } from "react-bootstrap";
+import { Container, ListGroup, Button, Form } from 'react-bootstrap';
+import { useState } from "react";
 export function Update() {
+
+    const list = [
+        { key: 'Last Name', value: 'Doe' },
+        { key: 'First Name', value: 'John' },
+        { key: 'Date of Birth', value: '01/01/2001'},
+        {key:'Position', value: 'Full Stack Developer'},
+        {key:'Location', value:'Maryland'}];
+
+    const [account, setAccount] = useState(list);
+    const [selectedItemKey, setSelectedItemKey] = useState(null);
+    const [newValue, setNewValue] = useState('')
+    const [showUpdateForm, setShowUpdateForm] = useState(false)
+
+    const updateValue = () => {
+        setAccount(prevData => prevData.map(item =>{
+            if(item.key === selectedItemKey){
+                return{...item, value: newValue };
+            }
+            return item;
+        }));
+        setNewValue('');
+        setShowUpdateForm(false);
+      };
+      
     return(
         <>
-    <Accordion defaultActiveKey={['0']} alwaysOpen>
-    <Accordion.Item eventKey="0">
-            <Accordion.Header>Employee Account #1</Accordion.Header>
-            <Accordion.Body>
-            <Container>
-                <Row> Last Name: Doe
-                </Row>
-                <Row>Firse Name: John</Row>
-                <Row>Date of Birth: 01/01/2001</Row>
-                <Row>Position: Junior Developer</Row>
-                <Row>State: Maryland </Row>
+            <br/><h1>Update Employee Accounts</h1>
 
-                </Container> 
-                <Button variant="primary">Add to This Employee Account</Button>{' '}
-
-            </Accordion.Body>
-        </Accordion.Item>
-
-        <Accordion.Item eventKey="1">
-            <Accordion.Header>Employee Account #2</Accordion.Header>
-            <Accordion.Body>
-            <Container>
-            <Row> Last Name: Goldman
-                </Row>
-                <Row>Firse Name: Moshe</Row>
-                <Row>Date of Birth: 02/02/2002</Row>
-                <Row>Position: Front End Developer</Row>
-                <Row>State: Maryland </Row>
-                </Container> 
-                <Button variant="primary">Add to This Employee Account</Button>{' '}
-
-            </Accordion.Body>
-        </Accordion.Item>
-        </Accordion>
-               <Outlet> </Outlet> 
+        <Accordion defaultActiveKey="" >
+        <Accordion.Item eventKey="0">
+                <Accordion.Header>Employee Account #1</Accordion.Header>
+                <Accordion.Body>
+                <Container>
+                    <ListGroup>
+                        {account.map((item) => (
+                            <ListGroup.Item key={item.key}>
+                            <h5>{item.key+":\t\t"}{item.value}</h5>
+                            <Button onClick={() => {
+                                setSelectedItemKey(item.key);
+                                 setShowUpdateForm(true);
+                                 }}>Update</Button>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                   
+                    {showUpdateForm && (
+                        <div>
+                            <Form.Group>
+                            <Form.Label>Enter Updated {selectedItemKey} Below:</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder={selectedItemKey}
+                                value={newValue}
+                                onChange={(e) => setNewValue(e.target.value)}/>
+                            </Form.Group>
+                            <Button onClick = {updateValue}>Update {selectedItemKey}</Button>
+                        </div>
+                    )}
+ 
+                </Container>
+ 
+                </Accordion.Body>
+            </Accordion.Item>
+            </Accordion>
         </>
     )
 }
