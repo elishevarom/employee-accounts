0046e55615ba252b656cb0fb27cc8754e7919945
ChanaMar2 <chanamarwick1@gmail.com>
2024-08-09 11:47:36 -0400

    Updated jest test

--- employee-accounts-web-app/src/__tests__/retrieve.test.js
+++ employee-accounts-web-app/src/__tests__/retrieve.test.js
@@ -8,11 +8,10 @@ test('fetches employees and displays them correctly', async () => {
     ok: true,
     json: () => Promise.resolve([
       { pk: 1, 'First Name': 'John', 'Last Name': 'Doe', Position: 'Manager', Address: '123 Main St', City: 'Anytown', State: 'CA', Zip: '12345', Phone: '123-456-7890', Email: 'john.doe@example.com' }
-    ]),
-  });
+    ]),});
 
   //creates a virtual DOM of the Retrieve component
-  render(<Retrieve />); 
+  render(<Retrieve />);
 
   await waitFor(() => screen.getByText('View Employee Profiles'));
 
@@ -24,24 +23,12 @@ test('fetches employees and displays them correctly', async () => {
 // Select the employee from the dropdown
   fireEvent.click(screen.getByRole('button', { name: /John Doe/i }));
 
-  // Wait for the component to fetch data and update
-  await waitFor(() => screen.getByText((content, element) => 
-  content.includes('Position: Manager')
-  ));
+  await waitFor(() => screen.getByText((content) =>  content.includes('Position: Manager')));
 
-  // AEnsure that the fetched employee data is displayed correctly
+  // Ensure that the fetched employee data is displayed correctly
   expect(screen.getByText('John Doe', { selector: '.card-title' })).toBeInTheDocument();
-  screen.getByText((content, element) => 
-    content.includes('Position: Manager'));
-  screen.getByText((content, element) => 
-    content.includes('Location: 123 Main St Anytown, CA 12345'));
-
-  screen.getByText((content, element) => 
-    content.includes('Phone: 123-456-7890'));
-  screen.getByText((content, element) => 
-    content.includes('Email: john.doe@example.com'));
-  // expect(screen.getByText('Position: Manager')).toBeInTheDocument();
-  // expect(screen.getByText('Location: 123 Main St Anytown, CA 12345')).toBeInTheDocument();
-  // expect(screen.getByText('Phone: 123-456-7890')).toBeInTheDocument();
-  // expect(screen.getByText('Email: john.doe@example.com')).toBeInTheDocument();
+  expect(screen.getByText((content) => content.includes('Position: Manager'))).toBeInTheDocument();
+  expect(screen.getByText((content) => content.includes('Location: 123 Main St Anytown, CA 12345'))).toBeInTheDocument();
+  expect(screen.getByText((content) => content.includes('Phone: 123-456-7890'))).toBeInTheDocument();
+  expect(screen.getByText((content) => content.includes('Email: john.doe@example.com'))).toBeInTheDocument();
 });
