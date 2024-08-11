ab6a4ef1709d46ae2c62e607d842d13392b0aa48
SerachS <152922973+SerachS@users.noreply.github.com>
2024-08-11 00:32:34 -0400

    Revert "67 custom bootstrap theme"

--- employee-accounts-web-app/src/pages/delete.jsx
+++ employee-accounts-web-app/src/pages/delete.jsx
@@ -5,7 +5,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
-import '../pages/styling.css';
 
 export const Delete = () => {
   const [employees, setEmployees] = useState([]);
@@ -14,46 +13,34 @@ export const Delete = () => {
     fetchData(); // Fetch data when component mounts
   }, []);
 
-
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://zx814esxf6.execute-api.us-east-1.amazonaws.com/default/getAllEmployeeAccounts');
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-        const data = await response.json();
-        const formattedEmployees = data.map(emp => ({
-          id: emp.pk,
-          firstName: emp['First Name'],
-          lastName: emp['Last Name'],
-          position: emp.Position,
-          location: (
-            <div>
-              {emp.Address}<br />
-              {emp.City}, {emp.State} {emp.Zip}
-            </div>
-          ),
-          phone: emp.Phone,
-          email: emp.Email,
-          isOpen: false
-        }));
-    
-        // Sort employees alphabetically by last name, then by first name
-        formattedEmployees.sort((a, b) => {
-          const lastNameComparison = a.lastName.localeCompare(b.lastName);
-          if (lastNameComparison === 0) {
-            return a.firstName.localeCompare(b.firstName);
-          }
-          return lastNameComparison;
-        });
-    
-        setEmployees(formattedEmployees);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        // Handle error state or logging
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://zx814esxf6.execute-api.us-east-1.amazonaws.com/default/getAllEmployeeAccounts');
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
       }
-    };
-    
+      const data = await response.json();
+      const formattedEmployees = data.map(emp => ({
+        id: emp.pk,
+        firstName: emp['First Name'],
+        lastName: emp.pk,
+        position: emp.Position,
+        location: (
+          <div>
+            {emp.Address}<br />
+            {emp.City}, {emp.State} {emp.Zip}
+          </div>
+        ),
+        phone: emp.Phone,
+        email: emp.Email,
+        isOpen: false
+      }));
+      setEmployees(formattedEmployees);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // Handle error state or logging
+    }
+  };
 
   const removeEmployee = async (employeeId) => {
     try {
@@ -95,92 +82,69 @@ export const Delete = () => {
   };
 
   return (
-    <div className="flex-column align-items-center justify-content-center overflow-auto min-vh-100 delete-background-image">
-      <div className="text-center">
-        <h1 className='m-5 fs-0 text-light pt-3'>DELETE EMPLOYEE ACCOUNT</h1>
-        <Container>
-          {employees
-            .sort((a, b) => {
-              const lastNameComparison = a.lastName.localeCompare(b.lastName);
-              if (lastNameComparison === 0) {
-                return a.firstName.localeCompare(b.firstName);
-              }
-              return lastNameComparison;
-            })
-            .map((employee) => (
-              <Col key={employee.id} className="mb-3">
-                <Row className='col-md-6'>
-                  <Accordion activeKey={employee.isOpen ? '0' : undefined}>
-                    <Accordion.Item eventKey="0" style={{ width: '100%' }}>
-                      <Accordion.Header
-                        className="text-white rounded-5"
-                        onClick={() => toggleAccordion(employee.id)}
-                      >
-                        {employee.lastName + ', ' + employee.firstName}
-                      </Accordion.Header>
-                      <Accordion.Body className='bg-primary'>
-                        <ul className='text-start' style={{ listStyleType: 'none' }}>
-                          <li>
-                            <Col>
-                              <Row className='mb-n1 text-light'>
-                                <h6>First Name:</h6>
-                              </Row>
-                              <Row className='ms-0 mb-3 text-info'>
-                                {employee.firstName}
-                              </Row>
-                              <Row className='mb-n1 text-light'>
-                                <h6>Last Name:</h6>
-                              </Row>
-                              <Row className='ms-0 mb-3 text-info'>
-                                {employee.lastName}
-                              </Row>
-                              <Row className='mb-n1 text-light'>
-                                <h6>Phone:</h6>
-                              </Row>
-                              <Row className='ms-0 mb-3 text-info'>
-                                {employee.phone}
-                              </Row>
-                              <Row className='mb-n1 text-light'>
-                                <h6>Email:</h6>
-                              </Row>
-                              <Row className='ms-0 mb-3 text-info'>
-                                {employee.email}
-                              </Row>
-                              <Row className='mb-n1 text-light'>
-                                <h6>Position:</h6>
-                              </Row>
-                              <Row className='ms-0 mb-3 text-info'>
-                                {employee.position}
-                              </Row>
-                              <Row className='mb-n1 text-light'>
-                                <h6>Location:</h6>
-                              </Row>
-                              <Row className='ms-n1 mb-3 text-info'>
-                                {employee.location}
-                              </Row>
-                            </Col>
-                            <Col style={{ paddingTop: '20px' }}>
-                              <Button
-                                type="button"
-                                style={{ backgroundColor: '#152235', color: 'white', borderColor: '#152235' }}
-                                onClick={(e) => handleConfirmation(e, employee.id, employee.firstName, employee.lastName)}
-                              >
-                                Delete Employee Profile
-                              </Button>
-                            </Col>
-                          </li>
-                        </ul>
-                      </Accordion.Body>
-                    </Accordion.Item>
-                  </Accordion>
-                </Row>
-              </Col>
-            ))}
-        </Container>
-      </div>
+    <div className="background-image">
+      <Alert style={{ boxShadow: '0 30px 20px 10px #152235', borderRadius: '0', backgroundColor: 'white', borderTop: '5px solid', borderBottom: '5px solid', color: '#5D9D67', borderColor: '#5D9D67', opacity: '90%' }}>
+        <h1 style={{ opacity: "100%", textShadow: '1px 1px 2px #152235', fontSize: '40px', fontWeight: 'bold', fontFamily: 'Times-New-Roman', letterSpacing: '10%', textTransform: 'uppercase' }}>Delete An Employee Account</h1>
+      </Alert>
+
+      <Container>
+        {employees.map((employee) => (
+          <Col key={employee.id} style={{ margin: '20px' }}>
+            <Row style={{ margin: '10px' }}>
+              <Accordion activeKey={employee.isOpen ? '0' : undefined}>
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header style={{ '--bs-accordion-active-bg': '#97a8ba', '--bs-accordion-btn-focus-box-shadow': 'none' }} className="accordion-header" onClick={() => toggleAccordion(employee.id)}>
+                    {employee.lastName + ', ' + employee.firstName}
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <ul style={{ listStyleType: 'none' }}>
+                      <li>
+                        <Row>
+                          <Col>
+                            <h6>First Name:</h6>
+                            {employee.firstName}
+                          </Col>
+                          <Col>
+                            <h6>Last Name:</h6>
+                            {employee.lastName}
+                          </Col>
+                          <Col>
+                            <h6>Location:</h6>
+                            {employee.location}
+                          </Col>
+                          <Col>
+                            <h6>Position:</h6>
+                            {employee.position}
+                          </Col>
+                          <Col>
+                            <h6>Phone:</h6>
+                            {employee.phone}
+                          </Col>
+                          <Col>
+                            <h6>Email:</h6>
+                            {employee.email}
+                          </Col>
+                        </Row>
+                        <Row style={{ paddingTop: '20px' }}>
+                          <Button
+                            type="button"
+                            style={{ backgroundColor: '#152235', color: 'white', borderColor: '#152235' }}
+                            onClick={(e) => handleConfirmation(e, employee.id, employee.firstName, employee.lastName)}
+                          >
+                            Delete Employee Profile
+                          </Button>
+                        </Row>
+                      </li>
+                    </ul>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </Row>
+          </Col>
+        ))}
+      </Container>
     </div>
   );
-  
 }
 
 export default Delete;
--- employee-accounts-web-app/src/pages/home.jsx
+++ employee-accounts-web-app/src/pages/home.jsx
@@ -1,28 +1,105 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import './customTheme.scss'; 
-import '../pages/styling.css';
+
+
+           
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Alert from 'react-bootstrap/Alert';
+import React, { useState, useEffect } from 'react';
+// import { Collapse } from 'bootstrap';
+
+
 
 export function Home() {
-    return (
-        <div className='home-background-image'>
-            <Container className="d-flex align-items-center justify-content-center min-vh-100">
-                <Col className="text-center">
-                    <Row className="mb-0">
-                        <h1 className="fs-xl fw-medium line-one text-light text-uppercase">Safe Account,</h1>
-                    </Row>
-                    <Row className="mb-0">
-                        <h1 className="fs-xxxl fw-bolder line-two text-secondary text-uppercase">Easy Access</h1>
-                    </Row>
-                    <Row className="justify-content-center">
-                        <Col xs={12} md={8} lg={7} className="d-flex justify-content-center">
-                            <h1 className="fs-2 line-three text-light text-wrap">
-                                access your secure employee accounts anytime, anywhere, with seamless convenience.
-                            </h1>
-                        </Col>
-                    </Row>
-                </Col>
-            </Container>
+   
+    return(
+        <>
+
+        <Alert>
+            <h1>Welcome to Employee Accounts</h1>
+ 
+        </Alert>
+        
+        
+        <Container>
+        <div className="row">
+
+        <Row>
+
+            <Col>
+            <Card style={{ width: '100%', height: '95%', margin: '10px', padding: '30px'}}>
+                <div background='primary'>
+                    <Card.Img variant="top" src="https://img.icons8.com/?size=100&id=60953&format=png&color=000000" width="50%"/>
+                </div>
+            
+                <div style={{background:'dark'}}>
+                <Card.Body>
+                <Card.Title>Add</Card.Title>
+                <Card.Text>
+                    Add a new employee's profile to the system.
+                </Card.Text>
+                <a href="add">
+                <Button class="mt-auto btn btn-primary" variant="primary">Select</Button>   
+                </a>
+                </Card.Body>
+                </div>
+            </Card>
+            </Col>
+
+            <Col>
+            <Card style={{ width: '100%', height: '95%', margin: '10px', padding: '30px'}}>
+            <Card.Img variant="top" src="https://img.icons8.com/?size=100&id=7695&format=png&color=000000" />
+                <Card.Body>
+                    <Card.Title>Retrieve</Card.Title>
+                    <Card.Text>
+                        View an existing employee's profile.
+
+                    </Card.Text>
+                    <a href="retrieve">
+                    <Button class="mt-auto btn btn-primary" variant="primary">Select</Button>   
+                    </a>
+                </Card.Body>
+               </Card>
+            </Col>
+
+            <Col>
+            <Card style={{ width: '100%', height: '95%', margin: '10px', padding: '30px'}}>
+            <Card.Img variant="top" src="https://img.icons8.com/?size=100&id=35635&format=png&color=000000" />
+            <Card.Body>
+                <Card.Title>Update</Card.Title>
+                <Card.Text>
+                    Update an existing employee's profile.
+                </Card.Text>
+                <a href="update">
+                <Button class="mt-auto" variant="primary">Select</Button>   
+                </a>
+            </Card.Body>
+            </Card>
+            </Col>
+
+            <Col>
+            <Card style={{ width: '100%', height: '95%', margin: '10px', padding: '30px'}}>
+                    <Card.Img variant="top" src="https://img.icons8.com/?size=100&id=67884&format=png&color=000000" />
+                    <Card.Body>
+                        <Card.Title>Delete</Card.Title>
+                        <Card.Text>
+                            Delete an existing employee's profile.
+                        </Card.Text>
+                        <a href="delete">
+                        <Button variant="primary">Select</Button>   
+                        </a>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
         </div>
-    );
-}
+
+        </Container>
+
+
+      </>
+)}
+
+
